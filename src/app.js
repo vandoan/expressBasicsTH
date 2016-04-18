7592411571a068fe1b3e716e6da03f7a020ffd47
Van Doan <vdoan09@gmail.com>
2016-04-17 22:57:31 -0400

    updating routes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+ 	posts 	= require('./mock/posts.json');
+
+
+var app = express();
+
+app.get('/', function(req,res){
+	res.send("<h1>i luv bananas</h1>");
+});
+
+app.get('/blog', function(req,res){
+	res.send(posts);
+})
+
+app.listen(3000, function(){
+	console.log("The frontend server is running on port 3000!")
+});
